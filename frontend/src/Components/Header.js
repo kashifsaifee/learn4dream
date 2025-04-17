72cf55d1a99819b44877b36c61c30b6cc99ef15c
Shubham <s30771747@gmail.com>
2025-04-17 16:18:46 +0530

    Add Header component and styles; update package dependencies

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Styles/Header.css';
+const Header = () => {
+  return (
+    <div className='header' >
+            shubham
+    </div>
+  )
+}
+
+export default Header
